cdc4be79f38d5c48a7c816ee805d65b82d8a80bf
Nico <heynicorivera@gmail.com>
2026-08-12 12:02:33 +0200

    Added JSON and index

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+STORAGE_FILE = "posts.json"
+
+
+def load_posts():
+    """Read all blog posts from the JSON storage file."""
+    with open(STORAGE_FILE, "r") as fileobj:
+        return json.load(fileobj)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
