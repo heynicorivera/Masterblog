a2eaa7da0aaa1a06ab54d229df8ec8bc9f010e81
Nico <heynicorivera@gmail.com>
2026-08-12 12:44:36 +0200

    Added the add route with form and POST handling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -13,10 +13,42 @@ def load_posts():
         return json.load(fileobj)
 
 
+def save_posts(posts):
+    """Write the given list of blog posts to the JSON storage file."""
+    with open(STORAGE_FILE, "w") as fileobj:
+        json.dump(posts, fileobj, indent=4)
+
+
+def generate_id(posts):
+    """Return a new unique id: one higher than the highest existing id."""
+    highest_id = 0
+    for post in posts:
+        if post["id"] > highest_id:
+            highest_id = post["id"]
+    return highest_id + 1
+
+
 @app.route('/')
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        blog_posts = load_posts()
+        new_post = {
+            "id": generate_id(blog_posts),
+            "author": request.form.get("author"),
+            "title": request.form.get("title"),
+            "content": request.form.get("content"),
+        }
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
